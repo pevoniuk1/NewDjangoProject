032ff1a52e7c4ccd09587dcc490e147cc26a612c
Perry Evoniuk <pevoniuk@hotmail.com>
2014-11-20 22:09:05 -0800

    First Commit

---
+++ src/NewDjangoProject/views.py
@@ -0,0 +1,23 @@
+'''
+Created on Nov 9, 2014                            s    
+@author: perryevoniuk
+'''
+from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def current_datetime (request):
+    now = datetime.datetime.now()
+    return render(request,'current_datetime.html', {'current_date':now})
+
+def hours_ahead (request, offset):
+   try:
+        offset = int(offset)
+   except ValueError:
+       raise Http404()
+   dt = datetime.datetime.now() + datetime.timedelta (hours=offset)
+   return render(request,'hours_ahead.html', {'hour_offset':offset,'next_time':dt})
+   
